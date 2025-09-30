82526044a94cf541036d5de1f094d1b64d2bd6fa
Maduki-tech <d.schlueter1011@gmail.com>
2025-09-30 20:06:47 +0200

    Added Fuzzy Search to Corfu lsp

--- doom/doom/config.el
+++ doom/doom/config.el
@@ -17,15 +17,8 @@
 ;;
 (setq doom-font (font-spec :family "JetBrainsMono Nerd Font Mono" :size 18 )
       doom-variable-pitch-font (font-spec :family "JetBrainsMono Nerd Font Mono" :size 18))
-;;
-;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
-;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
-;; refresh your font settings. If Emacs still can't find your font, it likely
-;; wasn't installed correctly. Font issues are rarely Doom issues!
 
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
+
 (setq doom-theme 'catppuccin)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
@@ -103,6 +96,10 @@
         corfu-auto-delay 0.1
         corfu-auto-prefix 1
         corfu-preselect 'first
+        completion-styles '(orderless)
+        completion-category-defaults nil
+        completion-category-overrides '((lsp-capf (styles orderless)))
+        orderless-matching-styles '(orderless-flex)
         )
   )
 
@@ -110,3 +107,9 @@
   (setq completion-styles '(orderless)
         completion-category-defaults nil
         completion-category-overrides '((file ()))))
+
+
+(after! lsp-mode
+  (setq lsp-completion-provider :none
+        lsp-completion-enable-additional-text-edit nil
+        lsp-completion-show-kind t))
