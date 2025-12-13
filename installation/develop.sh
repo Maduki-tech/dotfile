900539f90495742882da931e5db4bf804680f529
Maduki-tech <d.schlueter1011@gmail.com>
2025-12-13 09:41:03 +0100

    Updated config for new laptop

--- installation/develop.sh
+++ installation/develop.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Install dependencies
-sudo pacman -S --no-confirm --needed base-devel cmake ninja curl git stow tmux npm nodejs ripgrep fzf zsh go phthon python-pip docker
+sudo pacman -S --needed base-devel cmake ninja curl git stow tmux npm nodejs ripgrep fzf zsh go python python-pip docker
